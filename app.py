0afbea7b1c8a9790c8c42523cc69ceeea8c305b3
Reztocha <reztocha@gmail.com>
2020-07-04 20:42:25 -0400

    Snapshot update

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template,request)
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template(
-        'index.html'
-    )
+    if request.method == 'GET':
+        dta = request.form.get('imgdata')
+        return render_template(
+            'index.html',
+            dta = dta
+        )
+    else:
+        return render_template(
+            'index.html'
+        )
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
